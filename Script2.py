773d0563364c8ffdef25687c60cf128259edc10c
AlanCruz1 <alan_cruz@ciencias.unam.mx>
2024-02-16 02:57:05 -0600

    Acabé el segundo script

--- Script2.py
+++ Script2.py
@@ -1,11 +1,31 @@
- 
+#CANTIDAD DE VALLES
+def valles(recorrido):
+    nivel = 0 
+    valles = 0
+
+    for letra in recorrido:
+        if letra == 'U':
+            nivel += 1
+            """ SI PASÓ AL NIVEL DEL MAR TRAS UNA SUBIDA QUIERE DECIR QUE
+                ESTABA POR DEBAJO DEL NIVEL DEL MAR POR LO QUE SE ENCONTRABA
+                EN UN VALLE 
+            """
+            if nivel == 0:
+                valles += 1
+        else:
+            nivel -= 1
+
+    return valles
+
+#ÁRBOL BINARIO ORDENADO
+ #ME BASÉ EN EL TUTORIAL DE UN YOUTUBER HINDÚ JEJE
 class Nodo:
     def __init__(self, valor):
         self.valor = valor
         self.izquierda = None
         self.derecha = None
 
-class ArbolBinario:
+class ArbolBinarioOrdenado:
     def __init__(self, valor_raiz):
         self.raiz = Nodo(valor_raiz)
 
@@ -42,15 +62,25 @@ class ArbolBinario:
             self.postorden(nodo.derecha)
             print(nodo.valor, end=" ")
 
-# Ejemplo de uso:
+#FUNCIÓN  MAIN
 if __name__ == "__main__":
-    arbol = ArbolBinario(10)
-    arbol.agregar(5)
-    arbol.agregar(15)
-    arbol.agregar(3)
+    recorrido = "DDUUDDUUUDU" #MODIFICAR EL RECORRIDO DEL VIAJERO
+    print("Número de valles en el recorrido:", valles(recorrido))
+#ÁRBOL BINARIO DEFINIDDO DEFINIDO 
+    arbol = ArbolBinarioOrdenado(10)
     arbol.agregar(7)
+    arbol.agregar(3)
     arbol.agregar(12)
-    arbol.agregar(18)
+    arbol.agregar(2)
+    arbol.agregar(5)
+    arbol.agregar(10)
+    arbol.agregar(14)
+    arbol.agregar(1)
+    arbol.agregar(4)
+    arbol.agregar(6)
+    arbol.agregar(9)
+    arbol.agregar(11)
+    arbol.agregar(13)
 
     print("Recorrido del arbol:\n")
     print("Recorrido preorden:")
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Hay un ligero detalle que no tomé en cuenta hasta ahora y es que usé python 3.7.9 para la práctica por lo que el funcionamiento de los scripts debería funcionar perfectamente con eso.
